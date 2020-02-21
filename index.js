e87a6d6bc43dc52f080cbf68785b1715800f0c71
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2020-02-21 11:36:21 +0100

    add upcoming and events

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const getBattles = require('./lib/battles');
 const getCountries = require('./lib/countries');
 const getCountryToBattles = require('./lib/country-to-battles');
+const getUpcoming = require('./lib/upcoming');
+const getEvents = require('./lib/events');
 const fs = require('fs-extra');
 const path = require('path');
 
 async function main() {
   const battles = await getBattles();
-  //fs.writeJson(path.join(__dirname, 'output/battles.json'), battles);
+  fs.writeJson(path.join(__dirname, 'output/battles.json'), battles);
   const countryToBattles = await getCountryToBattles(battles.perYear);
   fs.writeJson(path.join(__dirname, 'output/country-to-battles.json'), countryToBattles);
+  const data = await getEvents();
+  fs.writeJson(path.join(__dirname, 'output/events.json'), data);
+  const used = process.memoryUsage().heapUsed / 1024 / 1024;
+  console.log(`The script used approximately ${Math.round(used * 100) / 100} MB.`);
 }
 
 main();
