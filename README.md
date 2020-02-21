b72de13515a7e07f7493cb30a0193d752ab636b4
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2020-02-21 14:46:20 +0100

    more CLI features

---
+++ README.md
@@ -0,0 +1,20 @@
+# Website data aggregator
+
+This library and CLI tool that aggregates the data needed for the website.
+
+## Usage
+
+### CLI
+
+- Download or clone this repo.
+- Install dependencies via `yarn install`
+- Install tool globally via `yarn global add file:$PWD`
+- Run the tool via `website-data-aggregator`
+
+### Library
+
+
+
+## License
+
+MIT
--- bin/cli.js
+++ bin/cli.js
@@ -1,20 +1,58 @@
+#!/usr/bin/env node
+
 const program = require('commander');
-const {getBattles} = require('../index');
+const {getBattles, getUpcoming, getCountries, getCountryToBattles,
+  getCountryToEvents, getCountryToJSONLD, getEvents} = require('../index');
+const path = require('path');
+const fs = require('fs-extra');
 
 main();
 
 async function main() {
   program
-    .requiredOption('-d, --data <data>', 'Data to aggregate')
-    .option('-o, --output', 'File to write output to');
+    .requiredOption('-d, --data <data>', 'Data to aggregate', validateDataOption)
+    .option('-o, --output <path>', 'File to write output to' , makeAbsolute);
 
   program.parse(process.argv);
 
+  let result;
+
   if (program.data === 'battles') {
-    const battles = await getBattles();
-    console.log(battles);
+    result = await getBattles();
+  } else if (program.data === 'upcoming') {
+    result = await getUpcoming();
+  } else if (program.data === 'events') {
+    result = await getEvents();
+  } else if (program.data === 'countries') {
+    result = await getCountries();
+  } else if (program.data === 'country-to-battles') {
+    const {perYear} = await getBattles();
+    result = await getCountryToBattles(perYear);
+  }
+
+  if (program.output) {
+    fs.writeJson(program.output, result);
   } else {
-    console.error(`Unknown value "${program.data}" for -d, --data.`);
+    console.log(result);
+  }
+}
+
+function validateDataOption(data) {
+  const validData = ['battles', 'events', 'upcoming', 'countries', 'country-to-battles', 'country-to-events'];
+  data = data.trim();
+
+  if (validData.includes(data)) {
+    return data;
+  } else {
+    console.error(`Unknown value "${data}" for -d, --data.`);
     process.exit(1);
   }
+}
+
+function makeAbsolute(filePath) {
+  if(! path.isAbsolute(filePath)) {
+    filePath = path.join(process.cwd(), filePath);
+  }
+
+  return filePath;
 }
