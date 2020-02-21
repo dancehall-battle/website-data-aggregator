2523c6e196f3cd52657819f39d75de30843fc88b
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2020-02-21 11:17:54 +0100

    added countries and country-to-battles

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const getBattles = require('./lib/battles');
+const getCountries = require('./lib/countries');
+const getCountryToBattles = require('./lib/country-to-battles');
 const fs = require('fs-extra');
 const path = require('path');
 
 async function main() {
   const battles = await getBattles();
-  fs.writeJson(path.join(__dirname, 'output/battles.json'), battles);
+  //fs.writeJson(path.join(__dirname, 'output/battles.json'), battles);
+  const countryToBattles = await getCountryToBattles(battles.perYear);
+  fs.writeJson(path.join(__dirname, 'output/country-to-battles.json'), countryToBattles);
 }
 
 main();
