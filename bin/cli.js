5d7b207b4ee1db33c8238775e65581dc2dd447aa
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2020-02-22 12:04:13 +0100

    support country to jsonld

--- bin/cli.js
+++ bin/cli.js
@@ -11,7 +11,8 @@ main();
 async function main() {
   program
     .requiredOption('-d, --data <data>', 'Data to aggregate', validateDataOption)
-    .option('-o, --output <path>', 'File to write output to' , makeAbsolute);
+    .option('-o, --output <path>', 'File to write output to' , makeAbsolute)
+    .option('-v, --verbose', 'Show debug info');
 
   program.parse(process.argv);
 
@@ -28,17 +29,36 @@ async function main() {
   } else if (program.data === 'country-to-battles') {
     const {perYear} = await getBattles();
     result = await getCountryToBattles(perYear);
+  } else if (program.data === 'country-to-events') {
+    const events = await getEvents();
+    result = await getCountryToEvents(events);
+  } else if (program.data === 'country-to-jsonld') {
+    const {originalQueryResults, perYear} = await getBattles();
+    const countryToBattles = await getCountryToBattles(perYear);
+    const events = await getEvents();
+    const countryToEvents = await getCountryToEvents(events);
+
+    result = await getCountryToJSONLD(originalQueryResults, countryToBattles, countryToEvents);
   }
 
   if (program.output) {
+    if (program.verbose) {
+      console.error(`Writing data to ${program.output}...`);
+    }
+
     fs.writeJson(program.output, result);
   } else {
     console.log(result);
   }
+
+  if (program.verbose) {
+    const used = process.memoryUsage().heapUsed / 1024 / 1024;
+    console.error(`Approximately ${Math.round(used * 100) / 100} MB memory was used.`);
+  }
 }
 
 function validateDataOption(data) {
-  const validData = ['battles', 'events', 'upcoming', 'countries', 'country-to-battles', 'country-to-events'];
+  const validData = ['battles', 'events', 'upcoming', 'countries', 'country-to-battles', 'country-to-events', 'country-to-jsonld'];
   data = data.trim();
 
   if (validData.includes(data)) {
