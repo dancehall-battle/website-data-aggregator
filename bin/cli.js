55a166fb23ad3f593ace6448d45705d26225556d
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2020-03-21 09:47:04 +0100

    wip #3

--- bin/cli.js
+++ bin/cli.js
@@ -52,6 +52,18 @@ async function main() {
     }
 
     result = await getCountryToEvents(events);
+  } else if (program.data === 'country-to-upcoming') {
+    let upcomingEvents = null;
+
+    if (program.cache) {
+      upcomingEvents = await getCachedFile(program.cache, 'upcoming.json', program.verbose);
+    }
+
+    if (!upcomingEvents) {
+      upcomingEvents = await getUpcoming();
+    }
+
+    result = await getCountryToEvents(upcomingEvents.data);
   } else if (program.data === 'country-to-jsonld') {
     let battles = null;
 
@@ -124,7 +136,7 @@ function validateDataOption(data) {
   const validData = ['battles', 'events', 'upcoming',
     'countries', 'country-to-battles', 'country-to-events',
     'country-to-jsonld', 'dancer-list', 'dancers',
-    'rankings'];
+    'rankings', 'country-to-upcoming'];
   data = data.trim();
 
   if (validData.includes(data)) {
